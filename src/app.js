d6cdf5ca93a79799362a87f1e072824cbb585737
Veerasudhan <sudhancheckmi98@gmail.com>
2020-06-19 01:31:04 +0530

    init commit

--- src/app.js
+++ src/app.js
@@ -18,7 +18,9 @@ if (process.env.NODE_ENV === "production") {
 
     app.get("*", (req, res) => {
         // res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-        res.sendFile(path.resolve("../client", "build", "index.html"));
+        res.sendFile(
+            path.resolve(__dirname, "../client", "build", "index.html")
+        );
     });
 }
 
